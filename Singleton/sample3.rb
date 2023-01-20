27094762b17eeec7295f21e876aa0ab1e3698bb6
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2023-01-20 19:54:27 +0900

    Singletonパターンのサンプル追加

---
+++ Singleton/sample3.rb
@@ -0,0 +1,26 @@
+class ClassBasedLogger
+  ERROR = 1
+  WARNING = 2
+  INFO = 3
+
+  @@log = File.open('log.txt', 'w')
+  @@level = WARNING
+
+  def self.error(msg)
+    @@log.puts(msg)
+    @@log.flush
+  end
+
+  def self.info(msg)
+    @@log.puts(msg) if @@level >= WARNING
+    @@log.flush
+  end
+
+  def self.level=(new_level)
+    @@level = new_level
+  end
+
+  def self.level
+    @@level
+  end
+end
